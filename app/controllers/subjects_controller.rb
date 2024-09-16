51d446992aa1ed6af438a4fab25237316f853dec
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-09-16 10:18:15 +0600

    Added Pages and Sections

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,20 +9,45 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new(name: 'default')
   end
 
-  def create 
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to(subjects_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
-  def update 
+  def update
+    @subject = Subject.find(params[:id])
+
+    if @subject.update(subject_params)
+      redirect_to(subject_path(@subject))
+    else
+      render('new')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    redirect_to subjects_path
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
