2d59d729e726e20fa4279413947a4a6f6522895b
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-09-23 12:03:21 +0600

    nesting pages in subjects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,13 @@ class PagesController < ApplicationController
 
   before_action :confirm_logged_in
   # find_subjects method runs before the other methods
+  before_action :find_subject
   before_action :find_subjects, only: %i[new create edit update]
-
   before_action :set_page_count, only: %i[new edit update]
 
   def index
-    @pages = Page.sorted
+    # @pages = Page.sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -16,14 +17,16 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
+    # @page = Page.new
+    @page = Page.new(subject_id: @subject.id)
   end
 
   def create
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = t('pages.create.success')
-      redirect_to pages_path
+      # redirect_to pages_path
+      redirect_to pages_path(subject_id: @subject.id)
     else
       render('new')
     end
@@ -39,7 +42,8 @@ class PagesController < ApplicationController
 
     if @page.update(page_params)
       flash[:notice] = t('pages.update.success')
-      redirect_to pages_path
+      # redirect_to page_path(@page)
+      redirect_to page_path(@page, subject_id: @subject.id)
     else
       @page_count = Page.count
       render('edit')
@@ -54,13 +58,17 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     @page.destroy
     flash[:notice] = t('pages.destroy.success', page_name: @page.name)
-    redirect_to pages_path
+    redirect_to pages_path(subject_id: @subject.id)
   end
 
   private
 
   def page_params
     params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
+  end
+
+  def find_subject
+    @subject = Subject.find(params[:subject_id])
   end
 
   def find_subjects
